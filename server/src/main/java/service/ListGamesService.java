f9ccabb8883651eeb305d84323081d142a904426
ZonkeyZebra <blackcec@yahoo.com>
2025-02-26 16:10:52 -0700

    phase 3: clean listGames

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -2,17 +2,12 @@ package service;
 
 import dataaccess.GameDAO;
 import model.GameData;
-import model.ListGamesResult;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 
 public class ListGamesService {
     private final GameDAO gameDataAccess;
     private Collection<GameData> games;
-    private Collection<ListGamesResult> gameList = new ArrayList<>();
-    private ListGamesResult result;
 
     public ListGamesService(GameDAO gameDataAccess) {
         this.gameDataAccess = gameDataAccess;
