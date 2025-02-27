8543ddf4a06175aa751a951004f679c5ef727973
ZonkeyZebra <blackcec@yahoo.com>
2025-02-26 21:17:38 -0700

    phase 3: clean clear and test

---
+++ server/src/test/java/service/ClearServiceTest.java
@@ -0,0 +1,39 @@
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClearServiceTest {
+    AuthDAO auths = new MemoryAuthDAO();
+    GameDAO games = new MemoryGameDAO();
+    UserDAO users = new MemoryUserDAO();
+    ClearService clearService = new ClearService(auths, games, users);
+
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        auths.setAuthData(new AuthData("token", "angela"));
+        games.createGame("Heaven");
+        users.createUser(new UserData("angela", "angel", "heaven@above,com"));
+    }
+
+    @Test
+    void clear() throws DataAccessException {
+        clearService.clear();
+        Assertions.assertNull(auths.getAuth("token"));
+        Assertions.assertNull(games.getGame(1));
+        Assertions.assertNull(users.getUser("angela"));
+    }
+
+    @Test
+    void clearFail() throws DataAccessException {
+        Assertions.assertNotNull(auths.getAuth("token"));
+        Assertions.assertNotNull(games.getGame(1));
+        Assertions.assertNotNull(users.getUser("angela"));
+    }
+}
