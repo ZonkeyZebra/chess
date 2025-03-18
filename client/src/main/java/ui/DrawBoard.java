11a0a16c81402455720b2eedbac465da61b04aaa
ZonkeyZebra <blackcec@yahoo.com>
2025-03-18 16:08:30 -0600

    start on drawing board

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,5 +1,151 @@
 package ui;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static ui.EscapeSequences.*;
+
 /// This class prints the board to the console
 public class DrawBoard {
+    // Board dimensions.
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
+
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+
+        drawHeaders(out);
+
+        drawTicTacToeBoard(out);
+
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void drawHeaders(PrintStream out) {
+
+        setBlack(out);
+
+        String[] headers = { "a", "b", "c", "d", "e", "f", "g", "h" };
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeader(out, headers[boardCol]);
+
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(1));
+            }
+        }
+
+        out.println();
+    }
+
+    private static void drawHeader(PrintStream out, String headerText) {
+        int prefixLength = 1;
+        int suffixLength = 0;
+
+        out.print(EMPTY.repeat(prefixLength));
+        printHeaderText(out, headerText);
+        out.print(EMPTY.repeat(suffixLength));
+    }
+
+    private static void printHeaderText(PrintStream out, String text) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_GREEN);
+
+        out.print(text);
+
+        setBlack(out);
+    }
+
+    private static void drawTicTacToeBoard(PrintStream out) {
+
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+
+            drawRowOfSquares(out);
+
+//            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+//                // Draw horizontal row separator.
+//                drawHorizontalLine(out);
+//                setBlack(out);
+//            }
+        }
+    }
+
+    private static void drawRowOfSquares(PrintStream out) {
+
+        for (int squareRow = 0; squareRow < 1; ++squareRow) {
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                setWhite(out);
+                int prefixLength = 0;
+                int suffixLength = 0;
+
+                //out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
+
+                if (isEvenNum(boardCol)) {
+                    setGray(out);
+                    out.print(EMPTY.repeat(1));
+                } else {
+                    out.print(EMPTY.repeat(prefixLength));
+                    printPiece(out, BLACK_PAWN);
+                    out.print(EMPTY.repeat(suffixLength));
+                }
+
+                setBlack(out);
+            }
+
+            out.println();
+        }
+    }
+
+    private static void drawHorizontalLine(PrintStream out) {
+
+        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS + (BOARD_SIZE_IN_SQUARES - 1);
+
+        for (int lineRow = 0; lineRow < 1; ++lineRow) {
+            setGray(out);
+            out.print(EMPTY.repeat(boardSizeInSpaces));
+
+            setBlack(out);
+            out.println();
+        }
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setGray(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void printPiece(PrintStream out, String piece) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_BLACK);
+
+        out.print(piece);
+
+        setWhite(out);
+    }
+
+    private static boolean isEvenNum(double num) {
+        if (num == 1) {
+            return false;
+        }
+        while (num > 1) {
+            num = num / 2;
+        }
+        if (num == 1 || num == 0) {
+            return true;
+        }
+        return false;
+    }
+
 }
