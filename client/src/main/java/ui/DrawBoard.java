6cc4f2a88601bd4d56440122c76eb6464bd6ff2b
ZonkeyZebra <blackcec@yahoo.com>
2025-03-19 14:36:14 -0600

    got rid of duplicate code

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -45,7 +45,7 @@ public class DrawBoard {
         int prefixLength = 1;
         int suffixLength = 0;
 
-        out.print(EMPTY.repeat(prefixLength));
+        //out.print(EMPTY.repeat(prefixLength));
         printHeaderText(out, headerText);
         out.print(EMPTY.repeat(suffixLength));
     }
@@ -87,21 +87,11 @@ public class DrawBoard {
 
     private static void printEvenRow(PrintStream out, int boardCol, int boardRow) {
         setLightGreen(out);
-        int prefixLength = 0;
-        int suffixLength = 0;
 
         if (isEvenNum(boardCol)) {
             setLightPurple(out);
             if (boardRow == 0) {
-                if (boardCol == 0) {
-                    printPiece(out, BLACK_ROOK, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_BLACK);
-                } else if (boardCol == 2) {
-                    printPiece(out, BLACK_BISHOP, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_BLACK);
-                } else if (boardCol == 4) {
-                    printPiece(out, BLACK_KING, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_BLACK);
-                } else if (boardCol == 6) {
-                    printPiece(out, BLACK_KNIGHT, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_BLACK);
-                }
+                getEvenPieces(out, boardCol, BLACK_ROOK, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_BLACK, BLACK_BISHOP, BLACK_KING, BLACK_KNIGHT);
             } else if (boardRow == 6) {
                 printPiece(out, WHITE_PAWN, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_WHITE);
             } else {
@@ -109,15 +99,7 @@ public class DrawBoard {
             }
         } else {
             if (boardRow == 0) {
-                if (boardCol == 7) {
-                    printPiece(out, BLACK_ROOK, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_BLACK);
-                } else if (boardCol == 5) {
-                    printPiece(out, BLACK_BISHOP, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_BLACK);
-                } else if (boardCol == 3) {
-                    printPiece(out, BLACK_QUEEN, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_BLACK);
-                } else if (boardCol == 1) {
-                    printPiece(out, BLACK_KNIGHT, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_BLACK);
-                }
+                getOddPieces(out, boardCol, BLACK_ROOK, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_BLACK, BLACK_BISHOP, BLACK_QUEEN, BLACK_KNIGHT);
             } else if (boardRow == 6) {
                 printPiece(out, WHITE_PAWN, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_WHITE);
             } else {
@@ -128,25 +110,39 @@ public class DrawBoard {
         setBlack(out);
     }
 
+    private static void getOddPieces(PrintStream out, int boardCol, String blackRook, String setBgColorLightGreen, String setTextColorBlack, String blackBishop, String blackQueen, String blackKnight) {
+        if (boardCol == 7) {
+            printPiece(out, blackRook, setBgColorLightGreen, setTextColorBlack);
+        } else if (boardCol == 5) {
+            printPiece(out, blackBishop, setBgColorLightGreen, setTextColorBlack);
+        } else if (boardCol == 3) {
+            printPiece(out, blackQueen, setBgColorLightGreen, setTextColorBlack);
+        } else if (boardCol == 1) {
+            printPiece(out, blackKnight, setBgColorLightGreen, setTextColorBlack);
+        }
+    }
+
+    private static void getEvenPieces(PrintStream out, int boardCol, String blackRook, String setBgColorLightPurple, String setTextColorBlack, String blackBishop, String blackKing, String blackKnight) {
+        if (boardCol == 0) {
+            printPiece(out, blackRook, setBgColorLightPurple, setTextColorBlack);
+        } else if (boardCol == 2) {
+            printPiece(out, blackBishop, setBgColorLightPurple, setTextColorBlack);
+        } else if (boardCol == 4) {
+            printPiece(out, blackKing, setBgColorLightPurple, setTextColorBlack);
+        } else if (boardCol == 6) {
+            printPiece(out, blackKnight, setBgColorLightPurple, setTextColorBlack);
+        }
+    }
+
     private static void printOddRow(PrintStream out, int boardCol, int boardRow) {
         setLightPurple(out);
-        int prefixLength = 0;
-        int suffixLength = 0;
 
         if (isEvenNum(boardCol)) {
             setLightGreen(out);
             if (boardRow == 1) {
                 printPiece(out, BLACK_PAWN, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_BLACK);
             } else if (boardRow == 7) {
-                if (boardCol == 0) {
-                    printPiece(out, WHITE_ROOK, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_WHITE);
-                } else if (boardCol == 2) {
-                    printPiece(out, WHITE_BISHOP, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_WHITE);
-                } else if (boardCol == 4) {
-                    printPiece(out, WHITE_KING, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_WHITE);
-                } else if (boardCol == 6) {
-                    printPiece(out, WHITE_KNIGHT, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_WHITE);
-                }
+                getEvenPieces(out, boardCol, WHITE_ROOK, SET_BG_COLOR_LIGHT_GREEN, SET_TEXT_COLOR_WHITE, WHITE_BISHOP, WHITE_KING, WHITE_KNIGHT);
             } else {
                 out.print(EMPTY.repeat(1));
             }
@@ -154,15 +150,7 @@ public class DrawBoard {
             if (boardRow == 1) {
                 printPiece(out, BLACK_PAWN, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_BLACK);
             } else if (boardRow == 7) {
-                if (boardCol == 7) {
-                    printPiece(out, WHITE_ROOK, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_WHITE);
-                } else if (boardCol == 5) {
-                    printPiece(out, WHITE_BISHOP, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_WHITE);
-                } else if (boardCol == 3) {
-                    printPiece(out, WHITE_QUEEN, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_WHITE);
-                } else if (boardCol == 1) {
-                    printPiece(out, WHITE_KNIGHT, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_WHITE);
-                }
+                getOddPieces(out, boardCol, WHITE_ROOK, SET_BG_COLOR_LIGHT_PURPLE, SET_TEXT_COLOR_WHITE, WHITE_BISHOP, WHITE_QUEEN, WHITE_KNIGHT);
             } else {
                 out.print(EMPTY.repeat(1));
             }
@@ -186,9 +174,14 @@ public class DrawBoard {
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void setGray(PrintStream out) {
+    private static void setLightGray(PrintStream out) {
         out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_LIGHT_GREY);
+    }
+
+    private static void setDarkGray(PrintStream out) {
+        out.print(SET_BG_COLOR_DARK_GREY);
+        out.print(SET_TEXT_COLOR_DARK_GREY);
     }
 
     private static void setBlack(PrintStream out) {
