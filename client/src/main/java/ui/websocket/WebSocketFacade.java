564aa65bd70f07c6e04029506ae666eaba588723
ZonkeyZebra <blackcec@yahoo.com>
2025-03-26 21:37:42 -0600

    initial code for wsfacade

--- client/src/main/java/ui/websocket/WebSocketFacade.java
+++ client/src/main/java/ui/websocket/WebSocketFacade.java
@@ -3,13 +3,81 @@ package ui.websocket;
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
+import com.google.gson.Gson;
+import exception.DataAccessException;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketFacade extends Endpoint {
-    private Session session;
+    private final Session session;
     private GameHandler gameHandler;
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
         //Endpoint requires this method, but you don't have to do anything
+    }
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws Exception {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    public void connect(int gameID, String authToken) throws DataAccessException {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public void makeMove(int gameID, String authToken) throws DataAccessException {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.MAKE_MOVE, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public void leaveGame(int gameID, String authToken) throws DataAccessException {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+            this.session.close();
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public void resignGame(int gameID, String authToken) throws DataAccessException {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+            this.session.close();
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 }
