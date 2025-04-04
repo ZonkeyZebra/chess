a2221e815e2e1e7d0bcd5aace08f6bddbdb389c5
ZonkeyZebra <blackcec@yahoo.com>
2025-04-04 12:06:19 -0600

    make move ws works on server

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,6 +1,8 @@
 package server.websocket;
 
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
 import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
 import dataaccess.MySqlAuthDAO;
@@ -30,6 +32,7 @@ public class WebSocketHandler {
     public void onMessage(Session session, String message) throws Exception {
         try {
             UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+            MakeMoveCommand moveCommand = new Gson().fromJson(message, MakeMoveCommand.class);
 
             AuthData authData = authDAO.getAuth(command.getAuthToken());
             String username = authData.username();
@@ -38,7 +41,7 @@ public class WebSocketHandler {
 
             switch (command.getCommandType()) {
                 case CONNECT -> connect(command.getGameID(), session, username);
-                case MAKE_MOVE -> makeMove(command.getGameID(), session, username, ((MakeMoveCommand) command));
+                case MAKE_MOVE -> makeMove(command.getGameID(), session, username, moveCommand);
                 case LEAVE -> leaveGame(command.getGameID(), session, username);
                 case RESIGN -> resignGame(command.getGameID(), session, username);
             }
@@ -63,7 +66,9 @@ public class WebSocketHandler {
     }
 
     public void makeMove(int gameID, Session session, String username, MakeMoveCommand command) throws IOException {
-        String message = String.format("%s made a move to %s.", username, command.getMove());
+        ChessPosition endMove = command.getMove().getEndPosition();
+        String move = String.format(convertColtoString(endMove.getColumn()) + convertRowtoString(endMove.getRow()));
+        String message = String.format("%s made a move to %s.", username, move);
         NotificationMessage notificationMessage = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
         connections.broadcast(session.toString(), gameID, new Gson().toJson(message), notificationMessage);
     }
@@ -84,5 +89,47 @@ public class WebSocketHandler {
 
     private void saveSession(int gameID, Session session) {
         connections.addSession(gameID, session);
+    }
+
+    private String convertRowtoString(int row) {
+        if (row == 1) {
+            return "1";
+        } else if (row == 2) {
+            return "2";
+        } else if (row == 3) {
+            return "3";
+        } else if (row == 4) {
+            return "4";
+        } else if (row == 5) {
+            return "5";
+        } else if (row == 6) {
+            return "6";
+        } else if (row == 7) {
+            return "7";
+        } else if (row == 8) {
+            return "8";
+        }
+        return "0";
+    }
+
+    private String convertColtoString(int col) {
+        if (col == 1) {
+            return "a";
+        } else if (col == 2) {
+            return "b";
+        } else if (col == 3) {
+            return "c";
+        } else if (col == 4) {
+            return "d";
+        } else if (col == 5) {
+            return "e";
+        } else if (col == 6) {
+            return "f";
+        } else if (col == 7) {
+            return "g";
+        } else if (col == 8) {
+            return "h";
+        }
+        return "0";
     }
 }
